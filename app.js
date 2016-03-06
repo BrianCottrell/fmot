99cc75b65cf45f43cb7e24318d1e6b516f7af29f
BrianCottrell <Brian_Cottrell@inbox.com>
2016-03-05 21:13:42 -0800

    Added end and share pages

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var nodemailer  = require('nodemailer');
 var port        = process.env.PORT || 8080;
 var Twit        = require('twit');
 
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'findmeontv@gmail.com',
+        pass: 'bc171212'
+    }
+});
+
 var twitter = new Twit({
     consumer_key: 'fp3ZiqvAppQygsAjE1m90ryUj',
     consumer_secret: 'bpJPzgtffmZzZl1jNMCRO8WG2vyaogDBf6KVDeG8xfpNjkrP4M',
@@ -17,7 +25,7 @@ var twitter = new Twit({
 });
 
 var params = {
-    status: 'Write your custom message here http://fmot.herokuapp.com/'
+    status: 'Write your custom message here'
 };
 
 var idenities = [
@@ -32,8 +40,6 @@ var idenities = [
     }
 ];
 
-var message = "Hello World!";
-
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
@@ -42,8 +48,9 @@ app.get('/', function(req, res){
 
 app.get('/send', function(req, res){
     params.status = req.query.message;
+    console.log(params.status);
     request({
-        url: 'http://hobnob.wirewax.com/public/video/8028022/',
+        url: 'http://hobnob.wirewax.com/public/video/8028050/',
         method: 'GET',
         headers: {
             'Authorization': 'Bearer aaf0cee4c7673d3a9000e94de82fce2499352a46'.toString('base64'),
@@ -56,23 +63,16 @@ app.get('/send', function(req, res){
         }else{
             console.log(body);
 
-            // twitter.post('statuses/update', params, function (err, data, response) {
-            //     console.log(data)
-            // });
-
-            var transporter = nodemailer.createTransport({
-                service: 'Gmail',
-                auth: {
-                    user: 'findmeontv@gmail.com',
-                    pass: 'bc171212'
-                }
+            twitter.post('statuses/update', params+' http://fmot.herokuapp.com/share', function (err, data, response) {
+                console.log(data)
             });
+
             var mailOptions = {
                 from: 'Find Me on TV!',
                 to: 'Brian_Cottrell@inbox.com',
-                subject: 'You have been found on TV!',
-                text: 'You have been found here: '+message,
-                html: '<b>Hello world ✔</b>'
+                subject: 'You or someone you are following was just featured on TV!',
+                text: params.status,
+                html: '<h1>Find Me on TV!</h1><b>'+params.status+'✔</b>'
             };
             transporter.sendMail(mailOptions, function(error, info){
                 if(error){
@@ -90,7 +90,7 @@ app.get('/send', function(req, res){
                         'Authorization': 'Basic ' + new Buffer('application\\6646ab62-5920-4274-b30e-cfce7d9f45bf:11VLa7eaPkKVY5au3y7PzQ==').toString('base64'),
                         'Content-Type' : 'application/json'
                     },
-                    body: "{\"message\": \"You have been found on TV!\"}"
+                    body: "{\"message\": \"You or someone you are following was just featured on TV!\"}"
                 }, 
                 function(error, response, body){
                     if(error){
@@ -104,12 +104,15 @@ app.get('/send', function(req, res){
         }
     }
     );
-    res.status(200).render('share_page');
+    res.status(200).render('end_page');
 });
 
-app.get('/test', function(req, res){
-    console.log('Test page loaded');
-    res.status(200).render('home_page');
+app.get('/end', function(req, res){
+    res.status(200).render('end_page');
+});
+
+app.get('/share', function(req, res){
+    res.status(200).render('share_page');
 });
 
 app.listen(port);
